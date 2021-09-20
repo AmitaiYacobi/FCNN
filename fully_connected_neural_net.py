3afac7b769e73b9da4dc0cd2ff7a766cf245fb90
Yacobi <amitaiyacobi@gmail.com>
2021-09-20 15:14:36 +0300

    project initialization

---
+++ fully_connected_neural_net.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+
+# data extraction:
+#   - extract results columns
+#   - create new data (new csv file) without the results colunms
+#   - clone the data from the file to a pandas table - optional (I will check this out)
+# feed forward:
+#   - initial weights accoding to some weights initialization method
+#   - feed the layers with the row including the calculation of the 
+#     non-linear function. 
+#   - print the expected result and the current result.
+#   - keep the result (the layer of the result will be
+#     represented as 10 neourons)
+# back propogation:
+#   - execute the back propogation algorithm according to the result
+#   - updtate the weights accordingly.
+#   - after finishing iterating the whole data, calculate the accuracy of the
+#     epoch and write it to a file.
+#   - 
+
+
+
+
