87791285dbec81c2418908608ee31bce70bfb5d4
Yacobi <amitaiyacobi@gmail.com>
2021-10-06 15:15:30 +0300

    added data extraction class and some other additions

--- fully_connected_neural_net.py
+++ fully_connected_neural_net.py
@@ -1,7 +1,42 @@
 import pandas as pd
 import numpy as np
 
+train_file_path = 'data/train.csv'
 
+class ExtractTrainData:
+    def __init__(self, path_to_data_file):
+        self.path_to_data_file = path_to_data_file
+        self.train_file = None
+    
+    def get_results_column(self):
+        if(self.train_file == None):
+            self.train_file  = pd.read_csv(data_file_path, header=None)
+            self.train_file.iloc[:, 0] 
+        
+
+    def get_train_file(self): 
+        if(self.train_file == None):
+            self.train_file  = pd.read_csv(data_file_path, header=None)
+        self.train_file = train_file.iloc[:, 1:]
+        return self.train_file      
+
+
+class FullyConnectedNeuralNet:
+    def __init__(self):
+        self.learnin_rate = 0.00001
+    
+    def RelU(self, number):
+        if number > 0:
+            return number
+        else:
+            return 0
+
+input_layer_size = len(train_file.columns)
+
+
+# first_hidden_layer = 2500 neurons
+# second_hidden_layer = 1536 neurons
+# results_layer = 10
 # data extraction:
 #   - extract results columns
 #   - create new data (new csv file) without the results colunms
