2b7a83a403451f21b36af52f449af66f409f3e24
Yacobi <amitaiyacobi@gmail.com>
2021-10-07 18:03:36 +0300

    another progress in FullyConnectedNeuralNet class

--- data_extraction.py
+++ data_extraction.py
@@ -6,17 +6,13 @@ train_file_path = 'data/train.csv'
 class ExtractTrainData:
     def __init__(self, path_to_data_file):
         self.path_to_data_file = path_to_data_file
-        self.train_file = None
+        self.train_data = pd.read_csv(self.path_to_data_file, header=None)
     
     def get_results_column(self):
-        if(self.train_file == None):
-            self.train_file  = pd.read_csv(self.path_to_data_file, header=None)
-            self.train_file.iloc[:, 0] 
+        return self.train_data.iloc[:, 0] 
 
-    def get_train_file(self): 
-        if(self.train_file == None):
-            self.train_file  = pd.read_csv(self.path_to_data_file, header=None)
-        self.train_file = self.train_file.iloc[:, 1:]
-        return self.train_file      
+    def get_train_data(self): 
+        return self.train_data.iloc[:, 1:]
 
-
+    def get_num_of_columns(self):
+        return len(self.get_train_data().columns)  
--- fully_connected_neural_net.py
+++ fully_connected_neural_net.py
@@ -1,17 +1,50 @@
 import pandas as pd
 import numpy as np
+import data_extraction
+
+
+from numpy.random import randn
+from math import sqrt
 
 class FullyConnectedNeuralNet:
     def __init__(self):
         self.learnin_rate = 0.00001
+        self.hidden_layers  = []
+        self.weights = [] 
+   
+    def create_layer(self, num_of_neurons):
+        self.hidden_layers.append(num_of_neurons)     
     
+    def weights_init(self):
+        hidden_layers = self.hidden_layers
+        input_to_hidden1 = randn(hidden_layers[0], hidden_layers[1]) * sqrt(2 / hidden_layers[0])   
+        hidden1_to_hidden2 = randn(hidden_layers[1], hidden_layers[2]) * sqrt(2 / hidden_layers[1])
+        hidden2_to_output = randn(hidden_layers[2], hidden_layers[3]) * sqrt(2 / hidden_layers[2])
+        self.weights = [input_to_hidden1, hidden1_to_hidden2, hidden2_to_output]
+
+    def change_weights(self, new_weights):
+        self.weights = new_weights
+
     def RelU(self, number):
         if number > 0:
             return number
         else:
             return 0
 
-input_layer_size = len(train_file.columns)
+
+
+def main():
+    data_extractor = data_extraction.ExtractTrainData('data/train.csv')
+    train_data = data_extractor.get_train_data()
+    input_layer_size = data_extractor.get_num_of_columns()
+    NN = FullyConnectedNeuralNet()
+    NN.create_layer(input_layer_size)
+    NN.create_layer(2500)
+    NN.create_layer(1536)
+    NN.create_layer(10)
+    NN.weights_init()
+
+main()
 
 
 # first_hidden_layer = 2500 neurons
