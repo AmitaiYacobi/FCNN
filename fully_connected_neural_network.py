e6eb42433aadb79910b8b5c18b38dd125db84e86
Yacobi <amitaiyacobi@gmail.com>
2021-10-22 15:57:26 +0300

    completed back_propagation function. needed to be tested

--- fully_connected_neural_network.py
+++ fully_connected_neural_network.py
@@ -15,15 +15,18 @@ class FullyConnectedNeuralNetwork:
         self.learnin_rate = 0.00001
         self.layers = []
         self.layers_size = []
+        self.layers_without_activation_func = []
         self.weights = []
         self.counter = 0
+        self.number_of_layers = 0
 
     def create_layer(self, num_of_neurons):
         self.layers_size.append(num_of_neurons)
+        self.number_of_layers += 1
 
     def weights_init(self):
         layers_size = self.layers_size
-        for i in range(0, len(layers_size) - 1):
+        for i in range(0, self.number_of_layers - 1):
             weight = randn(layers_size[i], layers_size[i+1]) * sqrt(2 / layers_size[i])
             self.weights.append(weight)
 
@@ -31,14 +34,17 @@ class FullyConnectedNeuralNetwork:
         self.weights = new_weights
 
     def feed_forward(self, input_layer, target):
-        layers = []
-        layers.append(input_layer)
-        for i in range(0, len(self.layers_size) - 1):
-            next_layer = np.dot(layers[i], self.weights[i])
-            if i < len(self.layers_size) - 2:
-                next_layer = np.vectorize(self.RelU)(next_layer)
-            layers.append(next_layer)
-        output_layer = layers[len(self.layers_size) - 1]
+        self.layers_without_activation_func = []
+        self.layers_without_activation_func.append(input_layer)
+        self.layers = []
+        self.layers.append(input_layer)
+        for i in range(0, self.number_of_layers - 1):
+            next_layer = np.dot(self.layers[i], self.weights[i])                         ## multiply matrices
+            self.layers_without_activation_func.append(next_layer)                       ## keeping the actual value of the neurons without activating the function over them.
+            if i < self.number_of_layers - 2:
+                next_layer = np.vectorize(self.RelU)(next_layer)                         ## activating activation function over the neurons.
+            self.layers.append(next_layer)
+        output_layer = self.layers[self.number_of_layers - 1]
         predicted_result = np.where(output_layer == output_layer.max())[0][0] + 1
         print(f"expected: {target} got: {predicted_result}\n")
         if target == predicted_result:
@@ -46,21 +52,35 @@ class FullyConnectedNeuralNetwork:
         return output_layer
 
     def back_propagation(self, output_layer, target, learning_rate):
-        # add counter for accuracy and apdate it every iteration in the epoch. After every epoch, calculate the accuracy of the epoch and write it to a file.
-        # after every epoch write the final weights of the epoch to a seperated file so it will be possible to use them later (in prediction).
+        errors_layers = []
+        updated_weights =[]
         correct_output = np.zeros(10)
         correct_output[target - 1] = 1
         error_output = correct_output - output_layer
-        print(error_output)
+        errors_layers.append(error_output)
+        for i in range(self.number_of_layers - 2, 0, -1):
+            current_layer_without_activation = self.layers_without_activation_func[i]
+            derivative_on_currnet_layer = np.vectorize(self.RelU_derivative)(current_layer_without_activation)
+            errors_of_layer_above = errors_layers[self.number_of_layers - 2 - i]
+            weights_i = np.transpose(self.weights[i])                                   ## multiply the errors of the above layer with the trandpose of the current weights matrix. 
+                                                                                        ## thats how we get the magnitude of the errors.
+            magnitude_of_errors = np.dot(errors_of_layer_above, weights_i)              ## the magnitude of the errors in the current layer 
+            errors_of_current_layer = derivative_on_currnet_layer * magnitude_of_errors
+            errors_layers.append(errors_of_current_layer)
+            layer_i = self.layers[i]
+            updated_weight = self.weights[i] + (learning_rate * (layer_i.reshape(self.layers_size[i], 1) * errors_of_layer_above))
+            ## print(self.layers[i]) to check whether the shape of the layers has changed. if it has, it is a problem. 
+            updated_weights.append(updated_weight)
+        return updated_weights
 
-    def train(self, train_data, targets, num_of_epochs=60, learning_rate):
+    def train(self, train_data, targets, learning_rate, num_of_epochs=60):
         i = 0
         self.weights_init()
         output_file = open("output.txt", "w")
-        while (True):
+        while (i <= num_of_epochs):
             self.counter = 0
             i += 1
-            for j in range(0, len(train_data)):
+            for j in range(0, len(train_data) - 1):
                 output_layer = self.feed_forward(train_data[j], targets[j])
                 new_weights = self.back_propagation(output_layer, targets[j], learning_rate)
                 self.change_weights(new_weights)
@@ -83,11 +103,16 @@ class FullyConnectedNeuralNetwork:
         accuracy = (self.counter / len(targets)) * 100
         print(accuracy)
 
-    def RelU(self, number):
-        if number > 0:
-            return number
+    def RelU(self, x):
+        return np.maximum(0, x)
+        
+    def RelU_derivative(self, x):
+        if x > 0:
+            return 1
         else:
             return 0
+
+
 
 
 def main():
@@ -103,12 +128,14 @@ def main():
     NN.create_layer(2500)
     NN.create_layer(1536)
     NN.create_layer(10)
-    NN.weights_init()
-    for k in range(0, len(NN.weights)):
-        print(NN.weights[k])
-        savetxt(f"layer_{k}_to_layer_{k+1}_weights.csv", NN.weights[k], delimiter=',')
-        print("######################################################################################################")
-        print(loadtxt(f"layer_{k}_to_layer_{k+1}_weights.csv", delimiter=',')) 
+    # NN.weights_init()
+    # NN.feed_forward(train_data[0], results[0])
+    # NN.train(train_data, results, 0.00001, 9)
+    # for k in range(0, len(NN.weights)):
+    #     print(NN.weights[k])
+    #     savetxt(f"layer_{k}_to_layer_{k+1}_weights.csv", NN.weights[k], delimiter=',')
+    #     print("######################################################################################################")
+    #     print(loadtxt(f"layer_{k}_to_layer_{k+1}_weights.csv", delimiter=',')) 
 
 
 main()
@@ -118,24 +145,3 @@ main()
 # hidden2 = np.dot(hidden1, self.weights[1])
 # hidden2 = np.vectorize(self.RelU)(hidden2)
 # output = np.dot(hidden2, self.weights[2])
-
-# first_hidden_layer = 2500 neurons
-# second_hidden_layer = 1536 neurons
-# results_layer = 10
-# data extraction:
-#   - extract results columns
-#   - create new data (new csv file) without the results colunms
-#   - clone the data from the file to a pandas table - optional (I will check this out)
-# feed forward:
-#   - initial weights accoding to some weights initialization method
-#   - feed the layers with the row including the calculation of the
-#     non-linear function.
-#   - print the expected result and the current result.
-#   - keep the result (the layer of the result will be
-#     represented as 10 neourons)
-# back propogation:
-#   - execute the back propogation algorithm according to the result
-#   - updtate the weights accordingly.
-#   - after finishing iterating the whole data, calculate the accuracy of the
-#     epoch and write it to a file.
-#   -
